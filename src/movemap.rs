126f4a45a87a645ff825303b58614c4ab763fb08
Hunter Chen <0x@hunterchen.ca>
2023-12-18 09:31:10 -0500

    fixed some errors with const functions

--- src/movemap.rs
+++ src/movemap.rs
@@ -1,4 +1,6 @@
-use std::arch::asm;
+#![allow(dead_code)]
+use std::arch::x86_64::_pext_u64;
+use self::{slider_pext::SLIDER_PEXT, slider_hash::SLIDER_HASH};
 mod check_between;
 mod pin_between;
 mod slider_h_cond;
@@ -12,12 +14,12 @@ mod attacks;
 mod offsets;
 mod seeds;
 
-struct SliderPext<'a> {
-  attack_ptr: &'a [u64],
+struct SliderPext {
+  attack_ptr: usize,
   mask: u64,
 }
 
-impl<'a> SliderPext<'a> {
+impl SliderPext {
   fn pext_u64_emulated(&self, val: u64, mask: u64) -> u64 {
     let mut res = 0;
     let mut bb = 1;
@@ -32,50 +34,38 @@ impl<'a> SliderPext<'a> {
     res
   }
 
-  fn _pext_u64(&self, val: u64, mask: u64) -> u64 {
-    let res: u64;
-    unsafe {
-      asm!(
-        "pext {res}, {val}, {mask}",
-        val = in(reg) val,
-        mask = in(reg) mask,
-        res = out(reg) res,
-      );
-    }
-    res
-  }
-
-  const fn new(offset: usize, mask: u64, slider_pext: &'a [u64]) -> Self {
+  const fn new(offset: usize, mask: u64) -> Self {
     Self {
-      attack_ptr: &slider_pext[offset..],
+      attack_ptr: offset,
       mask,
     }
   }
 
-  const fn get(&self, blocker: u64) -> u64 {
-    self.attack_ptr[self._pext_u64(blocker, self.mask) as usize] // Won't use emulated for now, I have a modern intel CPU.
+  fn get(&self, blocker: u64) -> u64 {
+    unsafe { SLIDER_PEXT[_pext_u64(blocker, self.mask) as usize + self.attack_ptr] }
   }
 }
 
-struct SliderHash<'a> {
-  attack_ptr: &'a [u64],
+struct SliderHash {
+  attack_ptr: usize,
   seed: u64,
   mask: u64,
 }
 
-impl<'a> SliderHash<'a> {
-  const fn new (offset: usize, seed: u64, mask: u64, slider_hash: &'a [u64]) -> Self {
+impl SliderHash {
+  const fn new (offset: usize, seed: u64, mask: u64) -> Self {
     Self {
-      attack_ptr: &slider_hash[offset..],
+      attack_ptr: offset,
       seed,
       mask,
     }
   }
 
-  const fn get(&self, blocker: u64) -> u64 {
-    self.attack_ptr[((self.mask | blocker).wrapping_mul(self.seed) >> (64 - 12)) as usize]
+  fn get(&self, blocker: u64) -> u64 {
+    SLIDER_HASH[(((self.mask | blocker) * self.seed) >> (64 - 12)) as usize + self.attack_ptr]
   }
 }
+
 
 // Slider Hash
 macro_rules! init_rook_slider_hashes {
@@ -85,13 +75,12 @@ macro_rules! init_rook_slider_hashes {
         offsets::ROOK_OFFSET[$idx] as usize,
         seeds::ROOK_SEED[$idx],
         !masks::RMASK[$idx],
-        &slider_hash::SLIDER_HASH
       )
     ),*]
   };
 }
 
-pub static HASH_ROOK_ATTACKS: [SliderHash; 64] = init_rook_slider_hashes!(
+static HASH_ROOK_ATTACKS: [SliderHash; 64] = init_rook_slider_hashes!(
   0, 1, 2, 3, 4, 5, 6, 7,
   8, 9, 10, 11, 12, 13, 14, 15,
   16, 17, 18, 19, 20, 21, 22, 23,
@@ -109,13 +98,12 @@ macro_rules! init_bishop_slider_hashes {
         offsets::BISHOP_OFFSET[$idx] as usize,
         seeds::BISHOP_SEED[$idx],
         !masks::BMASK[$idx],
-        &slider_hash::SLIDER_HASH
       )
     ),*]
   };
 }
 
-pub static HASH_BISHOP_ATTACKS: [SliderHash; 64] = init_bishop_slider_hashes!(
+static HASH_BISHOP_ATTACKS: [SliderHash; 64] = init_bishop_slider_hashes!(
   0, 1, 2, 3, 4, 5, 6, 7,
   8, 9, 10, 11, 12, 13, 14, 15,
   16, 17, 18, 19, 20, 21, 22, 23,
@@ -133,13 +121,12 @@ macro_rules! init_hash_rook_attacks_xray {
         offsets::ROOK_XRAY_OFFSET[$idx] as usize,
         seeds::ROOK_XRAY_SEED[$idx],
         !masks::RMASK[$idx],
-        &slider_hash::SLIDER_HASH
       )
     ),*]
   };
 }
 
-pub static HASH_ROOK_ATTACKS_XRAY: [SliderHash; 64] = init_hash_rook_attacks_xray!(
+static HASH_ROOK_ATTACKS_XRAY: [SliderHash; 64] = init_hash_rook_attacks_xray!(
   0, 1, 2, 3, 4, 5, 6, 7,
   8, 9, 10, 11, 12, 13, 14, 15,
   16, 17, 18, 19, 20, 21, 22, 23,
@@ -157,13 +144,12 @@ macro_rules! init_hash_bishop_attacks_xray {
         offsets::BISHOP_XRAY_OFFSET[$idx] as usize,
         seeds::BISHOP_XRAY_SEED[$idx],
         !masks::BMASK[$idx],
-        &slider_hash::SLIDER_HASH
       )
     ),*]
   };
 }
 
-pub static HASH_BISHOP_ATTACKS_XRAY: [SliderHash; 64] = init_hash_bishop_attacks_xray!(
+static HASH_BISHOP_ATTACKS_XRAY: [SliderHash; 64] = init_hash_bishop_attacks_xray!(
   0, 1, 2, 3, 4, 5, 6, 7,
   8, 9, 10, 11, 12, 13, 14, 15,
   16, 17, 18, 19, 20, 21, 22, 23,
@@ -181,13 +167,12 @@ macro_rules! init_pext_rook_attack {
       SliderPext::new(
         offsets::ROOK_OFFSET_PEXT[$idx] as usize,
         !masks::RMASK[$idx],
-        &slider_pext::SLIDER_PEXT
       )
     ),*]
   };
 }
 
-pub static PEXT_ROOK_ATTACKS: [SliderPext; 64] = init_pext_rook_attack!(
+static PEXT_ROOK_ATTACKS: [SliderPext; 64] = init_pext_rook_attack!(
   0, 1, 2, 3, 4, 5, 6, 7,
   8, 9, 10, 11, 12, 13, 14, 15,
   16, 17, 18, 19, 20, 21, 22, 23,
@@ -204,13 +189,12 @@ macro_rules! init_pext_bishop_attack {
       SliderPext::new(
         offsets::BISHOP_OFFSET_PEXT[$idx] as usize,
         !masks::BMASK[$idx],
-        &slider_pext::SLIDER_PEXT
       )
     ),*]
   };
 }
 
-pub static PEXT_BISHOP_ATTACKS: [SliderPext; 64] = init_pext_bishop_attack!(
+static PEXT_BISHOP_ATTACKS: [SliderPext; 64] = init_pext_bishop_attack!(
   0, 1, 2, 3, 4, 5, 6, 7,
   8, 9, 10, 11, 12, 13, 14, 15,
   16, 17, 18, 19, 20, 21, 22, 23,
@@ -227,13 +211,12 @@ macro_rules! init_pext_rook_attacks_xray {
       SliderPext::new(
         offsets::ROOK_XRAY_OFFSET_PEXT[$idx] as usize,
         !masks::RMASK[$idx],
-        &slider_pext::SLIDER_PEXT
       )
     ),*]
   };
 }
 
-pub static PEXT_ROOK_ATTACKS_XRAY: [SliderPext; 64] = init_pext_rook_attacks_xray!(
+static PEXT_ROOK_ATTACKS_XRAY: [SliderPext; 64] = init_pext_rook_attacks_xray!(
   0, 1, 2, 3, 4, 5, 6, 7,
   8, 9, 10, 11, 12, 13, 14, 15,
   16, 17, 18, 19, 20, 21, 22, 23,
@@ -250,13 +233,12 @@ macro_rules! init_pext_bishop_attacks_xray {
       SliderPext::new(
         offsets::BISHOP_XRAY_OFFSET_PEXT[$idx] as usize,
         !masks::BMASK[$idx],
-        &slider_pext::SLIDER_PEXT
       )
     ),*]
   };
 }
 
-pub static PEXT_BISHOP_ATTACKS_XRAY: [SliderPext; 64] = init_pext_bishop_attacks_xray!(
+static PEXT_BISHOP_ATTACKS_XRAY: [SliderPext; 64] = init_pext_bishop_attacks_xray!(
   0, 1, 2, 3, 4, 5, 6, 7,
   8, 9, 10, 11, 12, 13, 14, 15,
   16, 17, 18, 19, 20, 21, 22, 23,
@@ -266,6 +248,7 @@ pub static PEXT_BISHOP_ATTACKS_XRAY: [SliderPext; 64] = init_pext_bishop_attacks
   48, 49, 50, 51, 52, 53, 54, 55,
   56, 57, 58, 59, 60, 61, 62, 63
 );
+
 
 mod lookup_switch {
   use super::{attacks, slider_h_cond::get_slider_h_cond, slider_d1_cond::get_slider_d1_cond, slider_d2_cond::get_slider_d2_cond};
@@ -314,27 +297,27 @@ mod lookup_hash {
     super::attacks::KNIGHT_ATTACKS[square as usize]
   }
 
-  const fn rook(square: u64, occ: u64) -> u64 {
+  fn rook(square: u64, occ: u64) -> u64 {
     super::HASH_ROOK_ATTACKS[square as usize].get(occ)
   }
 
-  const fn rook_xray(square: u64, occ: u64) -> u64 {
+  fn rook_xray(square: u64, occ: u64) -> u64 {
     super::HASH_ROOK_ATTACKS_XRAY[square as usize].get(occ)
   }
 
-  const fn bishop(square: u64, occ: u64) -> u64 {
+  fn bishop(square: u64, occ: u64) -> u64 {
     super::HASH_BISHOP_ATTACKS[square as usize].get(occ)
   }
 
-  const fn bishop_xray(square: u64, occ: u64) -> u64 {
+  fn bishop_xray(square: u64, occ: u64) -> u64 {
     super::HASH_BISHOP_ATTACKS_XRAY[square as usize].get(occ)
   }
 
-  const fn queen(square: u64, occ: u64) -> u64 {
+  fn queen(square: u64, occ: u64) -> u64 {
     rook(square, occ) | bishop(square, occ)
   }
 
-  const fn queen_xray(square: u64, occ: u64) -> u64 {
+  fn queen_xray(square: u64, occ: u64) -> u64 {
     rook_xray(square, occ) | bishop_xray(square, occ)
   }
 }
@@ -348,27 +331,27 @@ mod lookup_pext {
     super::attacks::KNIGHT_ATTACKS[square as usize]
   }
 
-  const fn rook(square: u64, occ: u64) -> u64 {
+  fn rook(square: u64, occ: u64) -> u64 {
     super::PEXT_ROOK_ATTACKS[square as usize].get(occ)
   }
 
-  const fn rook_xray(square: u64, occ: u64) -> u64 {
+  fn rook_xray(square: u64, occ: u64) -> u64 {
     super::PEXT_ROOK_ATTACKS_XRAY[square as usize].get(occ)
   }
 
-  const fn bishop(square: u64, occ: u64) -> u64 {
+  fn bishop(square: u64, occ: u64) -> u64 {
     super::PEXT_BISHOP_ATTACKS[square as usize].get(occ)
   }
 
-  const fn bishop_xray(square: u64, occ: u64) -> u64 {
+  fn bishop_xray(square: u64, occ: u64) -> u64 {
     super::PEXT_BISHOP_ATTACKS_XRAY[square as usize].get(occ)
   }
 
-  const fn queen(square: u64, occ: u64) -> u64 {
+  fn queen(square: u64, occ: u64) -> u64 {
     rook(square, occ) | bishop(square, occ)
   }
 
-  const fn queen_xray(square: u64, occ: u64) -> u64 {
+  fn queen_xray(square: u64, occ: u64) -> u64 {
     rook_xray(square, occ) | bishop_xray(square, occ)
   }
 }
